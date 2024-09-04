6b97170ab26a3319bab9d17b1a6800e976572d7c
MateusChavito <mateus.chavito@hotmail.com>
2024-09-04 16:43:44 -0300

    Handling captured pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -5,9 +5,11 @@ import chess.ChessPiece;
 import chess.ChessPosition;
 
 import java.awt.*;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class UI {
@@ -39,30 +41,35 @@ public class UI {
         System.out.flush();
     }
 
-    public static ChessPosition readChessPosition(Scanner sc){
+    public static ChessPosition readChessPosition(Scanner sc) {
         try {
             String s = sc.nextLine();
             char column = s.charAt(0);
             int row = Integer.parseInt(s.substring(1));
             return new ChessPosition(column, row);
-        }
-        catch(RuntimeException e){
+        } catch (RuntimeException e) {
             throw new InputMismatchException(" Error reading ChessPosition. Valid values are from a1 to h8");
         }
     }
 
-    public static void printMatch(ChessMatch chessMatch){
+    public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
         printBoard(chessMatch.getPieces());
         System.out.println();
+        printCapturedPieces(captured);
+        System.out.println();
         System.out.println("Turn :" + chessMatch.getTurn());
-        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+        System.out.println("Waiting player: " + (chessMatch.getCurrentPlayer() == Color.WHITE ? "WHITE" : "BLACK"));
+
+        //System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+        //código antigo que estava com o erro java.awt.Color[r=255,g=255,b=255]
+
     }
 
 
     public static void printBoard(ChessPiece[][] pieces) {
-        for (int i=0; i<pieces.length; i++){
+        for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
-            for(int j=0; j<pieces.length; j++){
+            for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j], false);
             }
             System.out.println();
@@ -71,9 +78,9 @@ public class UI {
     }
 
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
-        for (int i=0; i<pieces.length; i++){
+        for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
-            for(int j=0; j<pieces.length; j++){
+            for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j], possibleMoves[i][j]);
             }
             System.out.println();
@@ -82,20 +89,32 @@ public class UI {
     }
 
     private static void printPiece(ChessPiece piece, boolean background) {
-        if(background){
+        if (background) {
             System.out.print(ANSI_BLUE_BACKGROUND);
         }
         if (piece == null) {
             System.out.print("-" + ANSI_RESET);
-        }
-        else {
+        } else {
             if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-            }
-            else {
+            } else {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
         System.out.print(" ");
+    }
+
+    private static void printCapturedPieces(List<ChessPiece> captured) {
+        List<ChessPiece> white = captured.stream().filter(x -> x.getColor() == Color.WHITE).collect(Collectors.toList());
+        List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
+        System.out.println("Captured pieces");
+        System.out.print("White: ");
+        System.out.print(ANSI_WHITE);
+        System.out.println(Arrays.toString(white.toArray()));
+        System.out.print(ANSI_RESET);
+        System.out.print("Black: ");
+        System.out.print(ANSI_YELLOW);
+        System.out.println(Arrays.toString(black.toArray()));
+        System.out.print(ANSI_RESET);
     }
 }
